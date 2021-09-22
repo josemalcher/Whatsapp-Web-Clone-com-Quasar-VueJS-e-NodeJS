5ec99a83109596bcafade272aded7caa4dd7a5b8
José Malcher Jr <contato@josemalcher.net>
2021-09-21 21:08:09 -0300

    15m - 3. Criando o frontend da aplicação com VueJS (Quasar para estilizações)

---
+++ whatsdev01/src/pages/Index.vue
@@ -0,0 +1,79 @@
+<template>
+  <q-page class="row items-center justify-center">
+    <div class="container image q-pa-xl">
+      <h5>Acesse o chat</h5>
+      <q-input
+        rounded
+        outlined
+        v-model="email"
+        label="Informe seu email"
+        bg-color="white"
+        class="q-mb-md"
+      ></q-input>
+      <q-btn
+        :ripple="false"
+        color="secondary"
+        label="Acessar Chat"
+        no-caps
+      ></q-btn>
+    </div>
+    <q-separator vertical></q-separator>
+    <div class="container q-pa-xl">
+      <h5>Cadastre-se</h5>
+      <q-input
+        rounded
+        outlined
+        v-model="name"
+        label="Informe seu Nome"
+        bg-color="white"
+        class="q-mb-md"
+      ></q-input>
+      <q-input
+        rounded
+        outlined
+        v-model="emailSegnUp"
+        label="Informe seu Email"
+        bg-color="white"
+        class="q-mb-md"
+      ></q-input>
+      <q-btn
+        :ripple="false"
+        color="secondary"
+        label="Cadastrar"
+        no-caps
+      ></q-btn>
+    </div>
+  </q-page>
+</template>
+
+<script>
+
+export default ({
+  name: 'PageIndex',
+  data() {
+    return {
+      email: '',
+      name: '',
+      emailSegnUp: ''
+    }
+  }
+})
+</script>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h5 {
+    color: #444444;
+  }
+
+  .q-btn,
+  .q-input {
+    width: 300px;
+  }
+}
+
+</style>
