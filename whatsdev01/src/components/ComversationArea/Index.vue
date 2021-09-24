2cfc7f605ecb871886dd152d8f14c8cea67fdbe9
José Malcher Jr <contato@josemalcher.net>
2021-09-24 09:07:10 -0300

    46m/80m - 3. Criando o frontend da aplicação com VueJS (Quasar para estilizações)

--- whatsdev01/src/components/ComversationArea/Index.vue
+++ whatsdev01/src/components/ComversationArea/Index.vue
@@ -1,15 +1,25 @@
 <template>
-  <div class="empty">
-
+  <div class="conversation-area">
+    <Header />
+    <div class="card-area"></div>
   </div>
 </template>
 
 <script>
+import Header from "components/ComversationArea/components/Header";
+
 export default {
-  name: "ConversationArea"
+  name: "ConversationArea",
+  components: {
+    Header
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.conversation-area {
+  height: 100vh;
+  width: 410px;
+  background-color: #FFF;
+}
 </style>
--- whatsdev01/src/components/ComversationArea/components/Header.vue
+++ whatsdev01/src/components/ComversationArea/components/Header.vue
@@ -4,16 +4,79 @@
       <q-avatar size="40px" class="q-px-md">
         <q-img src="https://i.pravatar.cc/40"></q-img>
       </q-avatar>
+      <div class="row items-center justify-center">
+        <q-btn
+          round
+          dense
+          unelevated
+          icon="history"
+          text-color="gray"
+          size="15px"
+          class="q-mr-sm"
+        >
+        </q-btn>
+        <q-btn
+          round
+          dense
+          unelevated
+          icon="chat"
+          text-color="gray"
+          size="12px"
+          class="q-mr-sm"
+        >
+        </q-btn>
+        <q-btn
+          round
+          dense
+          unelevated
+          icon="fas fa-ellipsis-v"
+          text-color="gray"
+          size="11px"
+          class="q-mr-sm"
+        >
+        </q-btn>
+      </div>
+    </div>
+    <div class="search full-width q-pa-sm">
+      <q-input
+        rounded
+        outlined
+        class="full-width"
+        v-model="search"
+        :dense="false"
+        label="Pesquisar ou Iniciar Conversa"
+        bg-color="white"
+      >
+        <template v-slot:prepend>
+          <q-icon name="search" size="20px"></q-icon>
+        </template>
+      </q-input>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Header"
+  name: "Header",
+  data(){
+    return{
+      search: ''
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.container-header {
+  height: 118px;
+  background-color: $gray-whats;
+  border-bottom: 1px solid $border-gray;
+}
+.container-actions{
+  border-right: 1px solid $border-gray;
+  height: 59px;
+}
+.search{
+  background-color: $light-gray;
+}
 </style>
