260cf458b6da1a2660df97119164033e51e7f0ac
José Malcher Jr <contato@josemalcher.net>
2021-09-27 09:09:49 -0300

    67m/80m - 3. Criando o frontend da aplicação com VueJS (Quasar para estilizações)

---
+++ whatsdev01/src/components/ChatArea/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="chat-area">
+    <div class="image full-width full-height">
+      <h5>Mensagens</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChatArea",
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.chat-area {
+  height: calc(100vh - 59px - 62px);
+  min-width: 800px;
+  width: calc(100vw - 410px);
+  background-color: $grey-background;
+}
+.image {
+  background-image: url("../../assets/background.png");
+  overflow: auto;
+  display: flex;
+  flex-direction: column-reverse;
+}
+</style>
--- whatsdev01/src/pages/RestrictArea.vue
+++ whatsdev01/src/pages/RestrictArea.vue
@@ -5,6 +5,7 @@
     </div>
     <div class="column" v-if="1 === 1">
       <TopBar :title="'Jose Malcher Jr.'"/>
+      <ChatArea/>
     </div>
     <Empty v-else/>
   </q-page>
@@ -14,14 +15,15 @@
 import Empty            from 'src/components/Empty/Index'
 import ComversationArea from 'src/components/ComversationArea/Index';
 import TopBar           from 'src/components/TopBar/Index'
-
+import ChatArea         from 'src/components/ChatArea/Index'
 
 export default {
   name: "RestrictArea",
   components: {
     Empty,
     ComversationArea,
-    TopBar
+    TopBar,
+    ChatArea
   }
 }
 </script>
